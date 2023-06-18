ee6a41b65f49a8666f7bc8ff423af87e5fecd989
Wejdi-Ben-Saad <bensaad.wejdi@gmail.com>
2023-06-18 17:43:35 +0200

    finished lab2:Exam Submission form UI finished

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -115,6 +115,19 @@ class Enrollment(models.Model):
     #    else:
     #        return False
 
+class Question(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    question_grade = models.IntegerField(default=0)
+    question_text = models.TextField()
+
+    def is_get_score(self, selected_ids):
+       all_answers = self.choice_set.filter(is_correct=True).count()
+       selected_correct = self.choice_set.filter(is_correct=True, id__in=selected_ids).count()
+       if all_answers == selected_correct:
+           return True
+       else:
+           return False
+
 
 #  <HINT> Create a Choice Model with:
     # Used to persist choice content for a question
@@ -123,6 +136,11 @@ class Enrollment(models.Model):
     # Indicate if this choice of the question is a correct one or not
     # Other fields and methods you would like to design
 # class Choice(models.Model):
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choise_content = models.TextField()
+    is_correct = models.BooleanField()
+
 
 # <HINT> The submission model
 # One enrollment could have multiple submission
@@ -132,3 +150,6 @@ class Enrollment(models.Model):
 #    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
 #    choices = models.ManyToManyField(Choice)
 #    Other fields and methods you would like to design
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
